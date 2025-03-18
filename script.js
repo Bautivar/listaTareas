0ced18eff2f6597e44866796aa5697a6aa0a83c0
Bautista <bautivargas@gmail.com>
2025-03-19 00:35:46 +0100

    Buscador en progreso

--- script.js
+++ script.js
@@ -1,53 +1,55 @@
 'use strict'
 // SELECCIONO LOS ELEMENTOS PRINCIPALES
-let inputTarea = document.querySelector("input#tarea");
-let checkbox = document.querySelector("input#checkbox");
-let botonAgregar = document.querySelector("button#agregarTarea");
-let listaOrdenada = document.querySelector("ol");
+const inputTarea = document.querySelector("input#tarea");
+const checkbox = document.querySelector("input#checkbox");
+const inputBuscador = document.querySelector("input#buscador");
+const botonAgregar = document.querySelector("button#agregarTarea");
+const listaOrdenada = document.querySelector("ol");
 
-
-// // LOS GUARDO EN UN DIV PARA LUEGO AGREGARLOS EN LAS TAREAS
-
-let tareaAgregada;
-inputTarea.addEventListener("input",e => {
-    tareaAgregada = e.target.value;
-});
-let vacio = false;
-document.addEventListener("keydown", e =>{
-    if (e.key === "Enter" && tareaAgregada.trim() != "") {
-       vacio = true;
-    }
-});
 // HACER LAS FUNCIONES POR SEPARADO Y LUEGO AGRGARLAS. DE ESTA MANERA SE PUEDE AGREGAR BOTONES CON ENTER
 // LOS EVENTOS PUEDEN TENER MAS EVENTOS DENTRO. Agrgar tareas crea los botones pero esos botones les tengo que dar un valor predeterminado que sea el correspondiente en cada caso. Asi se opera mas facil. Para los botones usar el target que especifica si se presionaron.
 // BAUTI, LA FUNCION QUE SE HACE CON EL BOTON AGREGAR, DECLARARLA AFUERA ASI LA PODES USAR CUANDO PRSIONES ENTER, PORQ SON FUNCIONES. KPO
-botonAgregar.addEventListener("click",(e) =>{
-    if (tareaAgregada.trim() != "" ) {
+function agregarTarea() {
+    if (tareaAgregada.trim() != "") {
+        // Creo una la lista y un span que guarda el nombre de la tarea
         let li = document.createElement("li");
-        li.textContent = tareaAgregada;
+        let span = document.createElement("span");
+        span.textContent = tareaAgregada;
+        li.appendChild(span);
         listaOrdenada.appendChild(li);  
-
+        
+        // Agrego clase importante si el ususario lo pide
         if (checkbox.checked) {
-            li.classList.add("tareaImportante");
+            li.classList.add("importante");
         }
+
         // Contenedor de botones
         let contenedorBotones = document.createElement("div");
         // BOTON IMPORTANTE
         const botonImportante = document.createElement("button");
-        botonImportante.classList.add("importante");
         botonImportante.textContent = "Importante";
         botonImportante.style.backgroundColor = "gold";
+        botonImportante.addEventListener("click",() =>{
+                li.classList.toggle("importante");
+        })
         // BOTON MODIFICAR
         const botonModificar = document.createElement("button");
-        botonModificar.classList.add("modificar");
         botonModificar.textContent = "Modificar";
         botonModificar.style.backgroundColor = "lightblue";
+        botonModificar.addEventListener("click",() => {
+            let texto = prompt("Cambie el valor del texto");
+            if (texto.trim() != "" && texto != undefined) {
+                span.textContent = texto;
+            }
+        })
         // BOTON ELIMINAR
         const botonEliminar = document.createElement("button");
         botonEliminar.textContent = "Eliminar";
-        botonEliminar.classList.add("eliminar");
         botonEliminar.style.backgroundColor = "lightblue";
-
+        botonEliminar.addEventListener("click",() => {
+                listaOrdenada.removeChild(li);
+        })
+        
         // Agrego los botones al contenedor y
         contenedorBotones.appendChild(botonModificar);
         contenedorBotones.appendChild(botonImportante);
@@ -57,9 +59,48 @@ botonAgregar.addEventListener("click",(e) =>{
     inputTarea.value = "";
     tareaAgregada = "";
     checkbox.checked = false;
+}
+
+let tareaAgregada;
+inputTarea.addEventListener("input",e =>tareaAgregada = e.target.value);
+
+// Agregar tareas con el boton agregar
+botonAgregar.addEventListener("click",agregarTarea);
+
+// Agregar tareas con enter
+document.addEventListener("keydown",e =>{
+    if (e.key === "Enter") {
+        agregarTarea();
+    }
 });
 
-let botonEli = document.querySelector("button.eliminar");
-botonEli.addEventListener("click",(e)=>{
-
+inputBuscador.addEventListener("input",(e) => {
+    let tareas = Array.from(listaOrdenada.children);
+    let spans = [];
+    for (const tarea of tareas) {
+        spans.push(tarea.firstElementChild);
+    }
+    let input = e.target.value;
+    let posicion = 0;
+    for (const letra of input) {
+        for (const span of spans) {
+            if (span.textContent[posicion] != letra) {
+                span.style.display = "none";
+            }
+        }
+        posicion++;
+    }
+    // if (listaOrdenada.children.length > 0) {
+    //     let tareas = Array.from(listaOrdenada.children);
+    //     let spans = [];
+    //     for (const tarea of tareas) {
+    //         spans.push(tarea.firstElementChild);
+    //         for (const span of spans) {
+    //                if (span.textContent === e.target.value) {
+    //                     // O puedo primero sacar el valor del primer input (primera letra) que se agrega, luego un for con ese input
+    //                     // for con numeros para comparar el valor de l input con las letras del texto
+    //                } 
+    //         }
+    //     }
+    // }
 });
